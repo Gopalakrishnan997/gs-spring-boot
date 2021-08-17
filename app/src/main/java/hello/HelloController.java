d33df6ee187890d3d411fde3eab868ead83c3361
Gopal <85742332+Gopalakrishnan997@users.noreply.github.com>
2021-08-17 16:30:29 +0530

    Update HelloController.java

--- app/src/main/java/hello/HelloController.java
+++ app/src/main/java/hello/HelloController.java
@@ -9,17 +9,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HelloController {
     
     @RequestMapping("/")
-public  void CreditCardDetails() {
+public  String CreditCardDetails() {
 
    	 YearMonth currentYearMonth = YearMonth.now();
    	    System.out.printf("Days in month year %s: No of days: %s \n",
    	        currentYearMonth, currentYearMonth.lengthOfMonth());
+   	   
    	    YearMonth creditCardExpiry = YearMonth.of(2078, Month.FEBRUARY);
    	    System.out.printf("Your credit card expires on %s: No of days: %s \n",
    	        creditCardExpiry, creditCardExpiry.lengthOfMonth());
-   	   
-	
-	
-
-    }   
+   	 String monthyear = "Days in month year:"+ currentYearMonth;
+   	 String noofdays = "No of days:"+currentYearMonth.lengthOfMonth();
+   	 String cardexpires = "Your credit card expires on:"+ creditCardExpiry;
+   	 String expiresdays = "No of days:"+creditCardExpiry.lengthOfMonth();
+   	   return monthyear+noofdays+cardexpires+expiresdays;
+	}  
 }
