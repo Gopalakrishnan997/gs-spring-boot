c3e74806281cc514a3d2e93ae9d5424fa3982ae8
Gopal <85742332+Gopalakrishnan997@users.noreply.github.com>
2021-08-17 14:46:32 +0530

    Update HelloController.java

--- app/src/main/java/hello/HelloController.java
+++ app/src/main/java/hello/HelloController.java
@@ -1,5 +1,7 @@
 package hello;
 
+import java.time.Month;
+import java.time.YearMonth;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,6 +10,15 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Hello from Spring Boot!";
+         
+    	 YearMonth currentYearMonth = YearMonth.now();
+    	    System.out.printf("Days in month year %s: No of days: %s \n",
+    	        currentYearMonth, currentYearMonth.lengthOfMonth());
+    	    YearMonth creditCardExpiry = YearMonth.of(2078, Month.FEBRUARY);
+    	    System.out.printf("Your credit card expires on %s: No of days: %s \n",
+    	        creditCardExpiry, creditCardExpiry.lengthOfMonth());
+
+
+        return currentYearMonth.lengthOfMonth();
     }   
 }
